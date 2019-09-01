59671fab61292fb84fd4429c0d7dafa09219a9f7
sam gaudet <samgaudet@samsMacmini.telus>
2019-09-01 15:22:28 -0600

    - created api for querying the database

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,11 @@ const queries = require('./queries');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
--- backend/queries.js
+++ backend/queries.js
@@ -1,5 +1,96 @@
+const Pool = require('pg').Pool;
 
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'my_database',
+  port: 54320,
+});
+
+//get all users
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM highscores', (error, results) => {
+    if(error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+}
 
 //get user by username & password
+const getUser = (request, response) => {
+  const { username, password } = request.body;
+
+  pool.query('SELECT * FROM highscores WHERE username = $1 AND password = $2', [username, password], (error, results) => {
+    if(error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+}
+
+//add new user
+const addUser = (request, response) => {
+  const { username, password } = request.body;
+
+  pool.query('INSERT INTO invitees (username, password) VALUES ($1, $2)', [username, password], (error, results) => {
+    if(error) {
+      throw error;
+    }
+    response.status(201).send(`User added with the username: ${username}`);
+  });
+}
+
+//edit high score
+const editScore = (request, response) => {
+  const newScore = request.params.score;
+  const { username, password } = request.body;
+
+  pool.query(
+    'UPDATE highscores SET highscore = $1 WHERE username = $2 AND password = $3',
+    [username, [password], newScore],
+    (error, results) => {
+      if(error) {
+        throw error;
+      }
+      response.status(200).send(`High score modified of user: ${username}`);
+    }
+  );
+}
+
+//remove user
+const removeInvitee = (request, response) => {
+  const { username, password } = request.body;
+  
+  pool.query(
+    'DELETE FROM highscore WHERE username = $1 AND password = $2', [username, password], (error, results) => {
+    if(error) {
+      throw error;
+    }
+    response.status(200).send(`Invitee removed with name ${name}`);
+    }
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //
